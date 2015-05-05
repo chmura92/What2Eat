19dbab9551e2e19931c0f94235e009b060474190
koziu <koziu92@gmail.com>
2015-05-05 20:54:36 +0200

    no message

--- What2Eat/Models/UserAtributes.cs
+++ What2Eat/Models/UserAtributes.cs
@@ -16,13 +16,6 @@ namespace What2Eat.Models
         public bool IsVegan { get; set; }
         [Required]
         public SexKind Sex { get; set; }
-        public Dictionary<string, double> ActivityLevel = new Dictionary<string, double>()
-        {
-            {"Little or no Exercise/ desk job", 1.2},
-            {"Light exercise/ sports 1 – 3 days/ week", 1.375},
-            {"Moderate Exercise, sports 3 – 5 days/ week", 1.55},
-            {"Heavy Exercise/ sports 6 – 7 days/ week", 1.725},
-            {"Very heavy exercise/ physical job/ training 2 x/ day", 1.9}
-        };
+     
     }
 }
