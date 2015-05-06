c30991c6ca23b2059cc54c4eed4005e7a93ba301
koziu <koziu92@gmail.com>
2015-05-06 02:02:16 +0200

    no message

--- What2Eat/Models/Product.cs
+++ What2Eat/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using What2Eat.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using What2Eat.Enums;
 
 namespace What2Eat.Models
 {
@@ -7,9 +8,9 @@ namespace What2Eat.Models
         public int ProductId { get; set; }
         public string Name { get; set; }
         public ProductKind ProductKind { get; set; }
-        public double KcalPer100g { get; set; }
-        public double ProteinsPer100g { get; set; }
-        public double CarbonitesPer100g { get; set; }
-        public double FatPer100g { get; set; }
+        public double? KcalPer100g { get; set; }
+        public double? ProteinsPer100g { get; set; }
+        public double? CarbonitesPer100g { get; set; }
+        public double? FatPer100g { get; set; }
     }
 }
